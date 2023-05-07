60f0e1797dcbb2778b6b2e3584c670609a09cc43
Dellon02 <jy.dellon@gmail.com>
2023-05-07 15:33:56 +0200

    button : lets go

--- asset/fizzBuzzIntro.js
+++ asset/fizzBuzzIntro.js
@@ -123,8 +123,8 @@ function fizzBuzzIntro(choix2){
         }
     }
 }
-function valid(){
-    if (fizzbuzz==2){
-        window.location.replace("http://127.0.0.1:5500/html/fizzBuzz.html");
-    }
-}
+// function valid(){
+//     if (fizzbuzz==2){
+//         window.location.replace("http://127.0.0.1:5500/html/fizzBuzz.html");
+//     }
+// }
