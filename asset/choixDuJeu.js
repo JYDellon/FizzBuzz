18ae35fd4a2ede41c7b333551ec61abc8183377b
Dellon02 <jy.dellon@gmail.com>
2023-05-17 19:08:29 +0200

    refonte du projet

--- asset/choixDuJeu.js
+++ asset/choixDuJeu.js
@@ -5,7 +5,11 @@
 
 
 function valider2(){
-
+    if (localStorage.getItem('NbreDeDiviseur') == 2){
+        window.location.href='https://jydellon.github.io/FizzBuzz/html/fizzBuzzChronoDeuxDiviseeurs.html'
+    }else if (localStorage.getItem('NbreDeDiviseur') == 1){
+        window.location.href='https://jydellon.github.io/FizzBuzz/html/fizzBuzzChronoUnDiviseur.html'
+    }
 }
 function valider3(){
     if (localStorage.getItem('NbreDeDiviseur') == 2){
