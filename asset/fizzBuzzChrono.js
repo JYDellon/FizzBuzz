e67959af6f13ee873fd26225762275ee2ac56102
Dellon02 <jy.dellon@gmail.com>
2023-05-18 20:34:59 +0200

    Gestion des erreurs 11

--- asset/fizzBuzzChrono.js
+++ asset/fizzBuzzChrono.js
@@ -41,7 +41,7 @@ onload=function initialisation1(){
     //                                             s2--;beep();
     //                                         }, 1000);
 
-    myInterval2 = setInterval(afficher,12.5)
+    myInterval2 = setInterval(afficher,14.29)
 }
 
 function afficher(){
@@ -49,7 +49,7 @@ function afficher(){
     document.getElementById('tempo').innerHTML = text[a];
     console.log(text[a]);
 
-    if (j<80){
+    if (j<70){
 
         j++;
         document.getElementById('tempo').style.fontSize = j+"px";
