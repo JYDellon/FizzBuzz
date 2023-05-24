e3b70171626b9b6cc5622156b1919ede0a7df832
Dellon02 <jy.dellon@gmail.com>
2023-05-24 11:02:55 +0200

    test changement de monde

--- asset/fizzBuzzChronoMonde01.js
+++ asset/fizzBuzzChronoMonde01.js
@@ -1,5 +1,6 @@
 
 
+var indiceUn;var indiceDeux;var indiceTrois;var indiceQuatre;var indiceCinq;var indiceSix;var indiceSept;var indiceHuit;var indiceNeuf;var indiceDix;
 var coupleNbre1Nbre2=[[2,3],[2,5],[2,7],[2,11],[3,5],[3,7],[3,11],[5,7],[5,11],[7,11]];
 var quantiteDeNbre1EtNbre2ParMonde=[[5,4,2,2,2],[7,5,4,3,2],[8,6,6,4,3],[10,7,8,5,4],[17,8,10,7,5],[19,10,12,9,6],[21,12,14,11,6],[25,14,16,12,8],[30,16,18,13,8],[34,17,20,13,8]];
 var quantiteDeFizzBuzzParNiveauEtParMonde=[[2,1,1,1,1,1,1,1,1,1],[4,2,2,1,1,1,1,1,1,1],[6,3,2,2,2,2,2,1,1,1],[8,4,3,2,3,2,2,1,1,1],[10,5,4,3,3,2,2,1,1,1],[12,6,4,3,4,3,3,2,1,1],[14,7,5,4,5,3,3,2,2,2],[16,8,6,4,5,4,3,2,2,2],[17,9,7,5,6,4,4,3,2,2],[17,10,8,5,7,5,4,3,2,2]];
@@ -64,7 +65,7 @@ function affectationDesFizzBuzz(){
     h=0;k=-50;
     console.log(mondeUnNiveau);
     if (localStorage.getItem('mondeUnNiveau') == NaN || localStorage.getItem('mondeUnNiveau') == null){
-        localStorage.setItem('mondeUnNiveau',1);
+        localStorage.setItem('mondeUnNiveau',10);
     }else{
         mondeUnNiveau=localStorage.getItem('mondeUnNiveau');
         indiceI=mondeUnNiveau-1;
